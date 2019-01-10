c63fe43433c1c7bebd157e8d0cbb26224ffee160
ludeeus <joasoe@gmail.com>
2019-01-10 12:42:33 +0100

    add exeption

--- furystoolbox/cli/hass.py
+++ furystoolbox/cli/hass.py
@@ -17,30 +17,34 @@ def breaking_change(url):
         if "(breaking change)" in line:
             raw_changes.append(line)
     for change in raw_changes:
-        this = {}
-        pull_request = str(change)
-        pull = pull_request.split('home-assistant/home-assistant/pull/')[1]
-        pull_request = pull.split('"')[0]
-        if pull_request not in control:
-            prlink = '{}{}'.format(pull_base, pull_request)
-            try:
-                component = str(change)
-                component = component.split('(<a href="/components/')[1]
-                component = component.split('/">')[0]
-            except:
-                component = ''
-            doclink = '{}{}'.format(comp_base, component)
-            description = str(change).split('  <li>')[1]
-            description = description.split('(<a ')[0].replace('</code>', '')
-            desc = description.replace('<code class="highlighter-rouge">', '')
-            description = desc.replace('\u2019', '`')
-            description = description.replace('\u201c', '')
-            description = description.replace('\u201d', '')
-            this['pull_request'] = pull_request
-            this['prlink'] = prlink
-            this['component'] = component
-            this['doclink'] = doclink
-            this['description'] = description
-            changes['data'].append(this)
-            control.append(pull_request)
+        if change[0:3] == '<p>':
+            pass
+        else:
+            this = {}
+            pull_request = str(change)
+            pull = pull_request.split('home-assistant/home-assistant/pull/')[1]
+            pull_request = pull.split('"')[0]
+            if pull_request not in control:
+                prlink = '{}{}'.format(pull_base, pull_request)
+                try:
+                    component = str(change)
+                    component = component.split('(<a href="/components/')[1]
+                    component = component.split('/">')[0]
+                except:
+                    component = ''
+                doclink = '{}{}'.format(comp_base, component)
+                desc = str(change).split('  <li>')[1]
+                desc = desc.split('(<a ')[0]
+                desc = desc.replace('</code>', '')
+                desc = desc.replace('<code class="highlighter-rouge">', '')
+                desc = desc.replace('\u2019', '`')
+                desc = desc.replace('\u201c', '')
+                desc = desc.replace('\u201d', '')
+                this['pull_request'] = pull_request
+                this['prlink'] = prlink
+                this['component'] = component
+                this['doclink'] = doclink
+                this['description'] = desc
+                changes['data'].append(this)
+                control.append(pull_request)
     print(json.dumps(changes, sort_keys=True, indent=4, ensure_ascii=True))
